29ffa76412f682c8848e8ef2e051f051967c6f59
German1127 <kleverk2023@gmail.com>
2024-08-01 14:26:48 -0400

    update task 4

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-User="$1"
-ps aux | grep "$User" | grep -v "grep" | awk '$5 > 0 && $6 > 0 {print $0}'
+User=$1
+ps -u $User -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,cmd | grep -v " 0 0" | grep -v "grep"
