66fa18bcf957f1d4f13f803f23964e3572d7cbe0
German1127 <kleverk2023@gmail.com>
2024-08-05 10:51:40 -0400

    update task 0

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo whois "$1" | awk /Registrant/ || /Admin/ || /Tech/ {print $1", " $2} > whois.csv
+whois "$1" | awk -F': ' /Registrant/ || /Admin/ || /Tech/ {print $1", " $2} > whois.csv
