40435b953eae0091403590dbede0bae1e503895a
German1127 <kleverk2023@gmail.com>
2024-09-04 10:48:05 -0400

    task 1

---
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+pass="$1"
+pass="${pass#'{xor}'}"
+decoded_pass=$(echo "$pass" | base64 -d)
+
+decoded_pass_xor=""
+for ((i = 0; i < ${#decoded_pass}; i++)); do
+    char="${decoded_pass:$i:1}"
+    ascii_value=$(printf "%d" "'$char")
+    xor_result=$(( ascii_value ^ 95 ))
+    decoded_pass_xor+="$(printf "$(printf '\\x%x' $xor_result)")"
+done
+
+echo -e "$decoded_pass_xor"
+
